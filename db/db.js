7bfee7d6d2d3cc3a2aab5607f4453bc0517ef530
ralexander-dev <ralexander_dev@outlook.com>
2024-09-27 19:28:06 -0400

    DB Connection test

---
+++ db/db.js
@@ -0,0 +1,43 @@
+const mysql = require('mysql2');
+const fs = require('fs');
+
+require('dotenv').config();
+
+const config = {
+    host:process.env.DB_URL, 
+    user:process.env.DB_USER, 
+    password:process.env.DB_PASS, 
+    database:process.env.DB_NAME, 
+    port:3306, 
+    ssl:{ca:process.env.DB_CERT}
+  };
+
+const conn = new mysql.createConnection(config);
+conn.connect(
+    function (err) {
+        if (err) {
+            console.log("!!! Cannot connect !!! Error:");
+            throw err;
+        }
+        else {
+            console.log("Connection established.");
+            readData();
+        }
+    });
+
+function readData(){
+    conn.query('SELECT * FROM user',
+        function (err, results, fields) {
+            if (err) throw err;
+            else console.log('Selected ' + results.length + ' row(s).');
+            for (i = 0; i < results.length; i++) {
+                console.log('Row: ' + JSON.stringify(results[i]));
+            }
+            console.log('Done.');
+        })
+    conn.end(
+        function (err) {
+            if (err) throw err;
+            else  console.log('Closing connection.')
+    });
+};
