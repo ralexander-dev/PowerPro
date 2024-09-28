da13b8017816a8718d748a92c6278049440ceba6
ralexander-dev <ralexander_dev@outlook.com>
2024-09-27 20:16:41 -0400

    DB connection test2

--- db/db.js
+++ db/db.js
@@ -3,41 +3,46 @@ const fs = require('fs');
 
 require('dotenv').config();
 
-const config = {
-    host:process.env.DB_URL, 
-    user:process.env.DB_USER, 
-    password:process.env.DB_PASS, 
-    database:process.env.DB_NAME, 
-    port:3306, 
-    ssl:{ca:process.env.DB_CERT}
-  };
-
-const conn = new mysql.createConnection(config);
-conn.connect(
-    function (err) {
-        if (err) {
-            console.log("!!! Cannot connect !!! Error:");
-            throw err;
-        }
-        else {
-            console.log("Connection established.");
-            readData();
-        }
-    });
-
-function readData(){
-    conn.query('SELECT * FROM user',
-        function (err, results, fields) {
-            if (err) throw err;
-            else console.log('Selected ' + results.length + ' row(s).');
-            for (i = 0; i < results.length; i++) {
-                console.log('Row: ' + JSON.stringify(results[i]));
-            }
-            console.log('Done.');
-        })
-    conn.end(
+function testDB(callback) {
+    const config = {
+        host:process.env.DB_URL, 
+        user:process.env.DB_USER, 
+        password:process.env.DB_PASS, 
+        database:process.env.DB_NAME, 
+        port:3306, 
+        ssl:{ca:process.env.DB_CERT}
+      };
+    
+    const conn = new mysql.createConnection(config);
+    conn.connect(
         function (err) {
-            if (err) throw err;
-            else  console.log('Closing connection.')
-    });
+            if (err) {
+                console.log("!!! Cannot connect !!! Error:");
+                throw err;
+            }
+            else {
+                console.log("Connection established.");
+                readData(callback);
+            }
+        });
+
+        function readData(callback){
+            conn.query('SELECT * FROM user',
+                function (err, results, fields) {
+                    if (err) throw err;
+                    else console.log('Selected ' + results.length + ' row(s).');
+                    for (i = 0; i < results.length; i++) {
+                        console.log('Row: ' + JSON.stringify(results[i]));
+                    }
+                    console.log('Done.');
+                    callback(results);
+                })
+            conn.end(
+                function (err) {
+                    if (err) throw err;
+                    else  console.log('Closing connection.')
+            });
+        };
 };
+
+module.exports = testDB;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const testDB = require('./db/db');
 
 // Set the public folder as root directory for static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -8,6 +9,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Define routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/users', (req, res) => {
+  testDB((data) => {
+      res.json(data);  // Send the data as JSON
+  });
 });
 
 // Start the server
