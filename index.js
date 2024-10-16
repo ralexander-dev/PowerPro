d7b025b45c4251ae65e02fabb957d2d5be4537ca
Russell Alexander <159663288+ralexander-dev@users.noreply.github.com>
2024-10-16 16:34:30 +0000

    'public/pages' changed to 'views', file extensions updated, and links updated

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  Description: 
   This file is the entry point for the application. It sets up the express server and defines the routes.
  Date Created: 2024-09-26
- Last Modified: 2024-10-12
+ Last Modified: 2024-10-16
 */
 
 // !!! development variable to switch between database and JSON file
@@ -25,20 +25,18 @@ app.set('view engine', 'hbs'); // set the view engine to HBS
 /* End Express app configuration */
 
 /* Route definitions */
-// static routes
-// home route -- serves the index.html file
-app.get('/', (req, res) => {
-  //res.sendFile(path.join(__dirname, 'public', 'index2.html'));
-  res.render('index', { title: 'Home' });
-});
-
-// signup route -- serves the signUp.html file
-app.get('/signup', (req, res) => {
-  res.render('signup');
-});
-
+// static GET routes
+app.get('/', (req, res) => { res.render('index'); }); // home route -- renders index.hbs
+app.get('/signup', (req, res) => { res.render('signup'); }); // signup route -- renders signup.hbs
+app.get('/about', (req, res) => { res.render('about'); }); // about route -- renders about.hbs
+app.get('/contact', (req, res) => { res.render('contact'); }); // contact route -- renders contact.hbs
+app.get('/shipping', (req, res) => { res.render('shipping'); }); // shipping route -- renders shipping.hbs
+app.get('/menTops', (req, res) => { res.render('men-tops'); }); // menTops route -- renders men-tops.hbs
+app.get('/menBottoms', (req, res) => { res.render('men-bottoms'); }); // menBottoms route -- renders men-bottoms.hbs
+app.get('/menShoes', (req, res) => { res.render('men-shoes'); }); // menShoes route -- renders men-shoes.hbs
 // signup POST route -- handles form submission
 app.post('/signup', authController.register);
+
 
 // data routes
 // users route -- serves users table from the database as JSON
@@ -63,6 +61,4 @@ app.get('/purchases', (req, res) => {
 
 // server startup
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
