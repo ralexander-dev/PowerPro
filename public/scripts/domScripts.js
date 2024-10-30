d8b8d982c7df6b41c9aaa77cef5ac1538692a84c
Russell Alexander <159663288+ralexander-dev@users.noreply.github.com>
2024-10-30 18:41:50 +0000

    Theme switcher working. Style updates.

--- public/scripts/domScripts.js
+++ public/scripts/domScripts.js
@@ -1,27 +1,39 @@
 /*
  File: domScripts.js (original: scripts.js)
- Author(s): Robert Matias
+ Author(s): Robert Matias, Russell Alexander
  Description: 
   This file contains funcionality for DOM manipulation.
  Date Created: 
- Last Modified: 
+ Last Modified: 10/30/2024
 */
 
-darkButton = document.getElementById('darkButton');
-lightButton = document.getElementById('lightButton');
-body = document.body;
+const darkButton = document.getElementById('darkButton');
+const lightButton = document.getElementById('lightButton');
+const body = document.body;
 
-darkButton.addEventListener('click', () => {
-  body.classList.replace('light', 'dark');
-  localStorage.setItem('theme', 'dark');
-});
+if(darkButton) {
+  darkButton.addEventListener('click', () => {
+    body.classList.replace('lightTheme', 'darkTheme');
+    localStorage.setItem('theme', 'darkTheme');
+  });
+}
 
-lightButton.addEventListener('click', () => {
-  body.classList.replace('dark', 'light');
-  localStorage.setItem('theme', 'light');
-});
+if (lightButton) {
+  lightButton.addEventListener('click', () => {
+    body.style.transition = 'background-color 1s';
+    body.style.transition = 'color 1s';
+    body.classList.replace('darkTheme', 'lightTheme');
+    localStorage.setItem('theme', 'lightTheme');
+  });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
+  const theme = localStorage.getItem('theme');
+  if (theme) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add('darkTheme'); // Default theme
+  }
   console.log("Website Loaded");
 });
 
