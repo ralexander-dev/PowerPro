0e3e6ccfa0859628aa3f2e0703952be362634580
Russell Alexander <159663288+ralexander-dev@users.noreply.github.com>
2024-11-04 13:05:59 +0000

    Added functionality for retrieving cart from production DB ! Not yet tested !

--- public/scripts/dataUtils/purchases.js
+++
@@ -1,7 +0,0 @@
-
-fetch('/purchases')
-  .then(res => res.json())
-  .then(data => {
-    const purchases = data.map(purchase => `<li>${purchase.user_ID}</li>`);
-    document.getElementById('purchases').innerHTML = purchases.join('');
-  });
